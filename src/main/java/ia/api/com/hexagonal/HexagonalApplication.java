a83a103218f74964d213e5411e60bbdce1c6c3ad
valdemirogomes <valdemiro.gomes.rodrigues@gmail.com>
2024-09-14 18:46:49 -0300

    removing beans from main class

--- src/main/java/ia/api/com/hexagonal/HexagonalApplication.java
+++ src/main/java/ia/api/com/hexagonal/HexagonalApplication.java
@@ -1,9 +1,8 @@
 package ia.api.com.hexagonal;
 
-import org.modelmapper.ModelMapper;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class HexagonalApplication {
@@ -11,8 +10,5 @@ public class HexagonalApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(HexagonalApplication.class, args);
 	}
-@Bean
-	public ModelMapper modelMapper(){
-		return new ModelMapper();
-}
+
 }
