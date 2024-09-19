550b6d2078ed96709e2abdea7d439cdcb001052f
valdemirogomes <valdemiro.gomes.rodrigues@gmail.com>
2024-09-19 09:57:08 -0300

    implementing tests

--- src/test/java/ia/api/com/hexagonal/core/service/TaskServiceTest.java
+++ src/test/java/ia/api/com/hexagonal/core/service/TaskServiceTest.java
@@ -1,4 +1,38 @@
 package ia.api.com.hexagonal.core.service;
 
+import ia.api.com.hexagonal.core.TaskService;
+import ia.api.com.hexagonal.core.domain.Task;
+import ia.api.com.hexagonal.core.ports.TaskRepositoryPort;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
 public class TaskServiceTest {
+    @InjectMocks
+    TaskService taskService;
+
+    @Mock
+    TaskRepositoryPort taskRepositoryPort;
+
+    Task task;
+
+    @BeforeEach
+    public void setUP(){
+        task = new Task(1,"876556","Implementar metodo de salvar", true);
+
+    }
+    @Test
+    void youMustCreateTask(){
+        when(taskRepositoryPort.createTask(task)).thenReturn(task);
+        Task result= taskService.createTask(task);
+        assertNotNull(result);
+        assertEquals(task,result);
+    }
 }
